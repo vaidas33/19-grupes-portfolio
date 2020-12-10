711f215c20a624e3c87d62243cdfefda3f4e8d0a
vaidas33 <73877931+vaidas33@users.noreply.github.com>
2020-12-10 20:54:53 +0200

    Initial commit

---
+++ js/main-home.js
@@ -0,0 +1,41 @@
+/***************
+ALL IMPORTS
+****************/
+/* header */
+/* hero */
+/* features */
+import { renderPremiumFeatures } from './components/premium-features/renderPremiumFeatures.js';
+import { premiumFeaturesData } from './data/premiumFeaturesData.js';
+/* app promo */
+/* app promo 2 */
+/* how works */
+/* gallery */
+/* testimonials */
+/* pricing */
+/* faq */
+/* team */
+/* app store */
+/* subscribe */
+/* contact */
+/* footer */
+
+/***************
+EXECUTION
+****************/
+/* header */
+/* hero */
+/* features */
+renderPremiumFeatures('#premium_features_block', premiumFeaturesData);
+
+/* app promo */
+/* app promo 2 */
+/* how works */
+/* gallery */
+/* testimonials */
+/* pricing */
+/* faq */
+/* team */
+/* app store */
+/* subscribe */
+/* contact */
+/* footer */
